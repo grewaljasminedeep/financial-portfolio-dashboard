9b13c62e4707b966d987b8f2b2042e9e24f3383a
Jasmine <grewaljasminedeep@gmail.com>
2026-08-07 15:39:41 -0400

    ..

--- include/DbConnection.h
+++ include/DbConnection.h
@@ -7,4 +7,14 @@
 struct DbConfig {
     std::string host = "localhost";
     int port = 5432;
+    std::string db = "portfolio_db";
+    std::string user = "trader";
+    std::string password = "secure_pass";
 };
+
+class DbConnection {
+    public:
+    explicit DbConnection(const DbConfig& cfg);
+    pqxx::Connection& connection();
+    private:
+}
