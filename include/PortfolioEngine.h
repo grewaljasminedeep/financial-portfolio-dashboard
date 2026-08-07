41f60c6aeb41b7fd84ebe9db81078985620c3fe8
Jasmine <grewaljasminedeep@gmail.com>
2026-08-07 16:07:33 -0400

    ..

--- include/PortfolioEngine.h
+++ include/PortfolioEngine.h
@@ -7,4 +7,16 @@
 struct AssetPosition {
     int asset_id;
     std::string ticker;
-}
+    double quantity; //net shares
+    double avg_cost; //average cost per share
+    double current_price; //latest price
+    double market_value; //quantity * current_price
+    double cost_basis; //quantity * avg_cost
+    double unrealized_pnl; //market_value - cost_basis
+};
+
+struct PortfolioSummary {
+    double total_market_value;
+    double total_cost_basis;
+    double total_unrealized_pnl;
+};
