95967850193bc23dca549825b0ea821799127764
Jasmine <grewaljasminedeep@gmail.com>
2026-08-07 15:10:25 -0400

    ......

--- db/seed_data.sql
+++ db/seed_data.sql
@@ -8,3 +8,13 @@ ON CONFLICT (ticker) DO NOTHING;
 
 -- Sample transactions (adjust asset_id based on the DB; using subqueries for safety)
 INSERT INTO transactions (asset_id, transaction_type, quantity, price_per_unit, executed_at)
+SELECT a.asset_id, 'BUY', 10, 150.00, '2025-01-15 10:30:00+00'
+FROM assets a WHERE a.ticker = 'AAPL';
+
+INSERT INTO transactions (asset_id, transaction_type, quantity, price_per_unit, executed_at)
+SELECT a.asset_id, 'BUY', 5, 300.00, '2025-02-10 11:00:00+00'
+FROM assets a WHERE a.ticker = 'MSFT';
+
+INSERT INTO transactions (asset_id, transaction_type, quantity, price_per_unit, executed_at)
+SELECT a.asset_id, 'BUY', 8, 140.00, '2025-03-05 09:45:00+00'
+FROM assets a WHERE a.ticker = 'GOOGL';
