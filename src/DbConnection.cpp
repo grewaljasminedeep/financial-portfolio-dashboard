90132c5bfe34d0a9d8e41318f1cc32791f19893c
Jasmine <grewaljasminedeep@gmail.com>
2026-08-07 15:55:17 -0400

    .

---
+++ src/DbConnection.cpp
@@ -0,0 +1,12 @@
+DbConnection::DbConnection(const DbConfig& cfg) {
+    std::string connInfo = "host=" + cfg.host +
+                           " port=" + std::to_string(cfg.port) +
+                           " dbname=" + cfg.db +
+                           " user=" + cfg.user +
+                           " password=" + cfg.password;
+
+        conn_ = std::make_unique<pqxx::Connection>(connInfo.c_str());
+        if (!conn_->is_open()) {
+            throw std::runtime_error("Failed to open database connection");
+        }
+}
