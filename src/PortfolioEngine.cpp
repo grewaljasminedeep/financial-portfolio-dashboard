8daa9058cdee99fb9da49ae184083e61dab3ab97
Jasmine <grewaljasminedeep@gmail.com>
2026-08-09 11:41:05 -0400

    .....

--- src/PortfolioEngine.cpp
+++ src/PortfolioEngine.cpp
@@ -35,6 +35,16 @@ std::vector<AssetPosition> PortfolioEngine::getPositions() {
 
     // Get latest prices
     std::unordered_map<int, double> latest_price;
-    std::string priceSql = "SELECT p.asset_id, p.close_price"
+    std::string priceSql = " SELECT p.asset_id, p.close_price"
                            " FROM prices p"
                            " JOIN (SELECT asset_id, MAX(price_date) AS max_date"
+                           " FROM prices GROUP BY(asset_id) x"
+                           " ON p.asset_id = x.asset_id AND p.price_date = x.max_date";
+
+    for (auto const& row : txn.query(std::string(priceSql))) {
+        int asset_id = row["asset_id"].as<int>();
+        double price = row["close_price"].as<double>();
+        latest_price[asset_id] = price;
+    }
+
+    // Get tickers
