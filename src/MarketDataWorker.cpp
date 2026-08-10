23329106c468feadc06e5d871993ca2200162b31
Jasmine <grewaljasminedeep@gmail.com>
2026-08-10 19:49:19 -0400

    ..

--- src/MarketDataWorker.cpp
+++ src/MarketDataWorker.cpp
@@ -8,4 +8,17 @@ MarketDataWorker::MarketDataWorker(DbConnection& db,
                                     const std::vector<std::string>& watchlist,
                                     int pollIntervalSec,
                                     QObject* parent) : QThread(parent), db_(db), watchlist_(watchlist),
-                                    
+                                    pollIntervalSec_(pollIntervalSec),
+                                    running_(true) {}
+
+                                    void MarketDataWorker::stop() {
+                                        running_ = false;
+                                    }
+
+                                    void MarketDataWorker::run() {
+                                        while (running_)
+                                        {
+                                            
+                                        }
+                                        
+                                    }
