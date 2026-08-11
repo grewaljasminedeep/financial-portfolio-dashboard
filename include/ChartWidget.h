b6863e8620123581bca3701dd298ecb8a074c7b1
Jasmine <grewaljasminedeep@gmail.com>
2026-08-11 19:41:40 -0400

    ..

--- include/ChartWidget.h
+++ include/ChartWidget.h
@@ -7,4 +7,15 @@
 class ChartWidget : public QWidget {
     Q_OBJECT
 public:
+    explicit ChartWidget(DbConnection& db, QWidget* parent = nullptr);
+    void setTicker(const QString& ticker);
+protected:
+    void paintEvent(QPaintEvent* event) override;
+private:
+    DbConnection& db_;
+    QString ticker_;
+
+    struct PricePoint {
+        
+    }
 }
