88c22678eb967a5fb56ac9fc1579cfc9ffa38dc5
Jasmine <grewaljasminedeep@gmail.com>
2026-08-10 20:51:39 -0400

    ..

--- include/PortfolioModel.h
+++ include/PortfolioModel.h
@@ -7,4 +7,13 @@
 class PortfolioModel : public QAbstractTableModel {
     Q_OBJECT
 public:
-}
+    explicit PortfolioModel(QObject* parent = nullptr);
+    void setPositions(const std::vector<AssetPosition>& positions);
+    const std::vector<AssetPosition>& positions() const { return positions_; }
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount (const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+private:
+    std::vector<AssetPosition> positions_;
+};
