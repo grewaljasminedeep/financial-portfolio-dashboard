2213595a331ff0b9f309ef9d46d9c1763b473938
Jasmine <grewaljasminedeep@gmail.com>
2026-08-11 20:36:57 -0400

    ..

--- src/ChartWidget.cpp
+++ src/ChartWidget.cpp
@@ -7,5 +7,14 @@
 ChartWidget::ChartWidget(DbConnection& db, QWidget* parent) : QWidget(parent), db_(db) {}
 
 void ChartWidget::setTicker(const QString& ticker) {
-    
+    ticker_ = ticker;
+    update();
+}
+
+std::vector<ChartWidget::PricePoint> ChartWidget::loadPrices(const QString& ticker) {
+    pqxx::Connection& conn = db_.connection();
+    pqxx::Work txn(conn);
+
+    std::string sql = "SELECT p.close_price"
+                      "FROM prices p"
 }
