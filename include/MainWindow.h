dcc510e873b7ae6777a22c059ebfa0a6a4efb6a4
Jasmine <grewaljasminedeep@gmail.com>
2026-08-11 21:41:12 -0400

    .

---
+++ include/MainWindow.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "DbConnection.h"
+#include "MarketDataWorker.h"
+#include "PortfolioModel.h"
+#include "ChartWidget.h"
+#include <QMainWindow>
+#include <QTableView>
+#include <QLineEdit>
