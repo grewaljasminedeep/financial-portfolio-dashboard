6aabf2b0374e3d3d7022cf12e18a8c9c877826f2
Jasmine <grewaljasminedeep@gmail.com>
2026-08-11 20:47:20 -0400

    ...

--- src/ChartWidget.cpp
+++ src/ChartWidget.cpp
@@ -17,4 +17,14 @@ std::vector<ChartWidget::PricePoint> ChartWidget::loadPrices(const QString& tick
 
     std::string sql = "SELECT p.close_price"
                       "FROM prices p"
+                      "JOIN assets a ON a.asset_id = p.asset_id"
+                      "WHERE a.ticker = $1"
+                      "ORDER BY p.price_date ASC";
+
+    std::vector<PricePoint> pts;
+    int i = 0;
+    for (auto const& row : txn.exec_params(sql, ticker.toStdString())) {
+        pts.push_back({i++, row["close_price"].as<double>()});
+    }
+    txn.commit();
 }
